b1a4a99821e9358c428366a271d1dc3a0ff2b82c
Brian Conn <59902169+bconn485@users.noreply.github.com>
2020-08-07 18:11:08 -0400

    Adding all Files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private FOV fov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 test = new Vector3(0f, 1.0f, 0.0f);
+        fov.SetOrigin(transform.position);
+        fov.SetAimDirection(test);
+        
+    }
+}
---
+++ Assets/Scripts/FOV.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Mesh mesh;
+    private Vector3 origin;
+    private float startingAngle;
+    private float fov;
+    void Start()
+    {
+         mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+    public void LateUpdate()
+    {
+
+        float fov = 90f;
+        int rayCount = 50;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 5f;
+        Vector3 origin = Vector3.zero;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+        vertices[1] = new Vector3(3, 0);
+        vertices[2] = new Vector3(0, -3);
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            Vector3 change = new Vector3(Mathf.Cos(angle * (Mathf.PI / 180f)), Mathf.Sin(angle * (Mathf.PI / 180f)));
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, change, viewDistance);
+            if (raycastHit2D.collider == null)
+            {
+                change = new Vector3(Mathf.Cos(angle * (Mathf.PI / 180f)), Mathf.Sin(angle * (Mathf.PI / 180f)));
+                vertex = origin + change * viewDistance;
+            }
+            else
+            {
+                vertex = raycastHit2D.point;
+            }
+
+            vertices[vertexIndex] = vertex;
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+    public void SetOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection)
+    {
+        Vector3 dir = aimDirection.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        startingAngle = n - fov / 2f;
+    }
+
+}
